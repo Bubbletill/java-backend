4a520340510cea051e73d4e152d03752cf7a4cf8
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-05-24 23:29:25 +0100

    Resume / suspend

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def stock_item():
     cur = cnx.cursor(dictionary=True)
 
     sql = "SELECT * FROM stock WHERE `category` = %s AND `code` = %s"
-    adr = (request.get_json()['category'], request.get_json()['code'], )
+    adr = (request.get_json()['category'], request.get_json()['code'],)
     cur.execute(sql, adr)
 
     result = cur.fetchone()
@@ -148,6 +148,7 @@ def stock_item():
     success = {"success": True}
     result.update(success)
     return jsonify(result), 200
+
 
 # POS
 @app.route('/pos/suspend', methods=['POST'])
@@ -170,12 +171,79 @@ def pos_suspend():
     cur = cnx.cursor()
 
     sql = "INSERT INTO `suspended` (`store`, `date`, `reg`, `oper`, `items`) VALUES (%s, %s, %s, %s, %s)"
-    adr = (request.get_json()['store'], request.get_json()['date'], request.get_json()['reg'], request.get_json()['oper'], request.get_json()['items'],)
+    adr = (
+    request.get_json()['store'], request.get_json()['date'], request.get_json()['reg'], request.get_json()['oper'],
+    request.get_json()['items'],)
     cur.execute(sql, adr)
 
     cnx.commit()
 
     return '', 200
+
+
+@app.route('/pos/resume', methods=['POST'])
+def pos_resume():
+    if 'token' not in request.get_json() or 'usid' not in request.get_json():
+        return '{"success": false, "message":"Incomplete request."}', 200
+
+    if request.get_json()['token'] not in accessTokens:
+        return '{"success": false, "message":"Invalid access token."}', 403
+
+    cnx = mysql.connector.connect(
+        host=mysql_host,
+        port=mysql_port,
+        user=mysql_user,
+        password=mysql_password,
+        database=mysql_database
+    )
+    cur = cnx.cursor(dictionary=True)
+
+    sql = "SELECT `items` FROM suspended WHERE `usid` = %s"
+    adr = (request.get_json()['usid'],)
+    cur.execute(sql, adr)
+
+    result = cur.fetchone()
+    if result is None:
+        return '{"success": false, "message":"Invalid USID."}', 200
+
+    success = {"success": True}
+    result.update(success)
+
+    sql = "DELETE FROM suspended WHERE `usid` = %s"
+    adr = (request.get_json()['usid'],)
+    cur.execute(sql, adr)
+
+    cnx.commit()
+
+    return result['items'], 200
+
+
+@app.route('/pos/listsuspended', methods=['POST'])
+def pos_listsuspended():
+    if 'token' not in request.get_json() or 'store' not in request.get_json():
+        return '{"success": false, "message":"Incomplete request."}', 200
+
+    if request.get_json()['token'] not in accessTokens:
+        return '{"success": false, "message":"Invalid access token."}', 403
+
+    cnx = mysql.connector.connect(
+        host=mysql_host,
+        port=mysql_port,
+        user=mysql_user,
+        password=mysql_password,
+        database=mysql_database
+    )
+    cur = cnx.cursor(dictionary=True)
+
+    sql = "SELECT `usid`, `date`, `reg`, `oper` FROM suspended WHERE `store` = %s"
+    adr = (request.get_json()['store'], )
+    cur.execute(sql, adr)
+
+    result = cur.fetchall()
+    if result is None:
+        return '{"success": false, "message":"No transactions found."}', 200
+
+    return jsonify(result), 200
 
 
 if __name__ == '__main__':
